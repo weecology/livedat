a1c3eb47ec810bd4e09bb294fbd7dfa3770234fe
Ethan White <ethan@weecology.org>
2018-04-17 11:18:19 -0400

    Create basic structure

---
+++ archive.R
@@ -0,0 +1,48 @@
+#' Update the version number
+#'
+#' By default update the minor version number since most changes are new data.
+#' If [major] or [patch] is in the last commit summary increment the matching
+#' version instead.
+
+repo <- git2r::repository(".")
+git2r::checkout(repo, branch = "master")
+git2r::remote_add(repo,
+                  name = "deploy",
+                  url = paste("https://",
+                              github_token,
+                              "@github.com/weecology/livedat.git",
+                              sep="")
+                  )
+cred <- git2r::cred_token("GITHUB_TOKEN")
+
+# Use a special user name so that it is clear which commits are automated
+git2r::config(repo,
+              user.email = "weecologydeploy@weecology.org",
+              user.name = "Weecology Deploy Bot")
+
+# Check the most recent commit for version instructions 
+last_commit <- git2r::commits(repo)[[1]]
+current_ver <- semver::parse_version(readLines("version.txt"))
+if (grepl("Merge pull request", last_commit@summary)){
+    last_commit <- git2r::commits(repo)[[2]]
+}
+
+if (grepl("\\[no version bump\\]", last_commit@summary)) {
+  new_ver <- current_ver
+} else if (grepl("\\[major\\]", last_commit@summary)) {
+  new_ver <- semver::increment_version(current_ver, "major", 1L)
+} else if (grepl("\\[patch\\]", last_commit@summary)) {
+  new_ver <- semver::increment_version(current_ver, "patch", 1L)
+} else {
+  new_ver <- semver::increment_version(current_ver, "minor", 1L)
+}
+
+writeLines(as.character(new_ver), "version.txt")
+
+travis_build <- Sys.getenv("TRAVIS_BUILD_NUMBER")
+commit_message <- paste("Update data and trigger archive: Travis Build",
+                        travis_build,
+                        "[skip ci]")
+git2r::add(repo, "*")
+git2r::commit(repo, message = commit_message)
+git2r::push(repo, name = "deploy", refspec = "refs/heads/master", credentials = cred)
---
+++ datascript.R
@@ -0,0 +1,1 @@
+print("Have this script run whatever data cleaning you do")
